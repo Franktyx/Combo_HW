337ee99834f1dd5f908c72fc69d68ad491282aa8
crystalqujingjing <jingjingqu@jingjings-MacBook-Pro.local>
2015-05-28 13:48:18 -0700

    Initial Commit

---
+++ Combo_HW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Combo_HW
+//
+//  Created by jingjing qu on 5/28/15.
+//  Copyright (c) 2015 TYX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
